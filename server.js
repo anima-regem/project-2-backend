a7d4d4ee2b1412e7c05ad11c66bcf6a07b1369d7
anima-regem <vichukartha@gmail.com>
2023-03-24 07:41:52 +0530

    fix: makes video and audio endpoints open

--- server.js
+++ server.js
@@ -1,28 +1,72 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const transcript = require("./transcript.js");
 
-app.get('/video', (req, res) => {
-  const filePath = path.join(__dirname, 'video.mp4');
+app.use(cors());
+
+app.get("/video/:id/audio", (req, res) => {
+  console.log("audio");
+  const audioFile = "audio.webm";
+  const fs = require("fs");
+  const audioStream = fs.createReadStream(audioFile);
+
+  const range = req.headers.range;
+  const fileSize = fs.statSync(audioFile).size;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+    const fileStream = fs.createReadStream(audioFile, { start, end });
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "audio/webm",
+    };
+    res.writeHead(206, headers);
+    fileStream.pipe(res);
+  } else {
+    const headers = {
+      "Content-Length": fileSize,
+      "Content-Type": "audio/webm",
+    };
+    res.writeHead(200, headers);
+    audioStream.pipe(res);
+  }
+});
+
+app.get("/video/:id/subtitles", (req, res) => {
+
+  const sub = transcript[1];
+  res.json(sub);
+});
+
+app.get("/video/:id", (req, res) => {
+  console.log("video");
+  const filePath = path.join(__dirname, "video.mp4");
   const stat = fs.statSync(filePath);
   const fileSize = stat.size;
   const range = req.headers.range;
 
   if (range) {
     // If the request includes a range header, parse the start and end bytes
-    const parts = range.replace(/bytes=/, '').split('-');
+    const parts = range.replace(/bytes=/, "").split("-");
     const start = parseInt(parts[0], 10);
     const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
 
     // Set the headers for the partial content response
-    const chunkSize = (end - start) + 1;
+    const chunkSize = end - start + 1;
     const file = fs.createReadStream(filePath, { start, end });
     const headers = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunkSize,
-      'Content-Type': 'video/mp4'
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunkSize,
+      "Content-Type": "video/mp4",
     };
 
     // Send the partial content response
@@ -31,8 +75,8 @@ app.get('/video', (req, res) => {
   } else {
     // If the request does not include a range header, send the entire file
     const headers = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4'
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4",
     };
 
     // Send the entire file
@@ -41,6 +85,44 @@ app.get('/video', (req, res) => {
   }
 });
 
+app.get("/audio/:id/subtitles", (req, res) => {
+  const sub = transcript[0];
+  res.json(sub);
+});
+
+app.get("/audio/:id", (req, res) => {
+  console.log("audio-new");
+  const audioFile = "audio1.webm";
+  const fs = require("fs");
+  const audioStream = fs.createReadStream(audioFile);
+
+  const range = req.headers.range;
+  const fileSize = fs.statSync(audioFile).size;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+    const fileStream = fs.createReadStream(audioFile, { start, end });
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "audio/webm",
+    };
+    res.writeHead(206, headers);
+    fileStream.pipe(res);
+  } else {
+    const headers = {
+      "Content-Length": fileSize,
+      "Content-Type": "audio/webm",
+    };
+    res.writeHead(200, headers);
+    audioStream.pipe(res);
+  }
+});
+
 app.listen(8000, () => {
-  console.log('Server listening on port 8000');
+  console.log("Server listening on port 8000");
 });
---
+++ transcript.js
@@ -0,0 +1,579 @@
+const transcript1 = [
+  {
+    time: "00:00:04",
+    text: "You think I'm pretty",
+  },
+  {
+    time: "00:00:06",
+    text: " Without any makeup on ",
+  },
+  {
+    time: "00:00:08",
+    text: " You think I'm funny ",
+  },
+  {
+    time: "00:00:10",
+    text: " when I tell the punch line wrong ",
+  },
+  {
+    time: "00:00:12",
+    text: " I know you get me ",
+  },
+  {
+    time: "00:00:14",
+    text: " So I let my walls come down, down ",
+  },
+  {
+    time: "00:00:20",
+    text: " Before you met me ",
+  },
+  {
+    time: "00:00:22",
+    text: " I was all right ",
+  },
+  {
+    time: "00:00:23",
+    text: " But things were kinda heavy ",
+  },
+  {
+    time: "00:00:26",
+    text: " You brought me to life ",
+  },
+  {
+    time: "00:00:27",
+    text: " Now every February ",
+  },
+  {
+    time: "00:00:30",
+    text: " You'll be my Valentine, Valentine ",
+  },
+  {
+    time: "00:00:35",
+    text: " Let's go all the way tonight ",
+  },
+  {
+    time: "00:00:39",
+    text: " No regrets, just love ",
+  },
+  {
+    time: "00:00:43",
+    text: " We can dance, until we die ",
+  },
+  {
+    time: "00:00:47",
+    text: " You and I will be young forever ",
+  },
+  {
+    time: "00:00:52",
+    text: " You make me ",
+  },
+  {
+    time: "00:00:54",
+    text: " Feel like I'm livin' a teenage dream ",
+  },
+  {
+    time: "00:00:58",
+    text: " The way you turn me on ",
+  },
+  {
+    time: "00:01:00",
+    text: " I can't sleep ",
+  },
+  {
+    time: "00:01:02",
+    text: " Let's run away ",
+  },
+  {
+    time: "00:01:03",
+    text: " And don't ever look back ",
+  },
+  {
+    time: "00:01:06",
+    text: " Don't ever look back ",
+  },
+  {
+    time: "00:01:08",
+    text: " My heart stops ",
+  },
+  {
+    time: "00:01:10",
+    text: " When you look at me ",
+  },
+  {
+    time: "00:01:12",
+    text: " Just one touch ",
+  },
+  {
+    time: "00:01:14",
+    text: " Now, baby, I believe this is real ",
+  },
+  {
+    time: "00:01:18",
+    text: " So take a chance ",
+  },
+  {
+    time: "00:01:19",
+    text: " And don't ever look back ",
+  },
+  {
+    time: "00:01:22",
+    text: " Don't ever look back ",
+  },
+  {
+    time: "00:01:24",
+    text: " We drove to Cali ",
+  },
+  {
+    time: "00:01:26",
+    text: " And got drunk on the beach ",
+  },
+  {
+    time: "00:01:28",
+    text: " Got a motel ",
+  },
+  {
+    time: "00:01:30",
+    text: " And built a fort out of sheets ",
+  },
+  {
+    time: "00:01:32",
+    text: " I finally found you ",
+  },
+  {
+    time: "00:01:34",
+    text: " My missing puzzle piece ",
+  },
+  {
+    time: "00:01:37",
+    text: " I'm complete ",
+  },
+  {
+    time: "00:01:39",
+    text: " Let's go all the way tonight ",
+  },
+  {
+    time: "00:01:43",
+    text: " No regrets, just love ",
+  },
+  {
+    time: "00:01:47",
+    text: " We can dance until we die ",
+  },
+  {
+    time: "00:01:51",
+    text: " You and I will be young forever ",
+  },
+  {
+    time: "00:01:56",
+    text: " You make me ",
+  },
+  {
+    time: "00:01:58",
+    text: " Feel like I'm livin' a teenage dream ",
+  },
+  {
+    time: "00:02:02",
+    text: " The way you turn me on ",
+  },
+  {
+    time: "00:02:04",
+    text: " I can't sleep ",
+  },
+  {
+    time: "00:02:06",
+    text: " Let's run away ",
+  },
+  {
+    time: "00:02:07",
+    text: " And don't ever look back ",
+  },
+  {
+    time: "00:02:10",
+    text: " Don't ever look back ",
+  },
+  {
+    time: "00:02:12",
+    text: " My heart stops ",
+  },
+  {
+    time: "00:02:15",
+    text: " When you look at me ",
+  },
+  {
+    time: "00:02:16",
+    text: " Just one touch ",
+  },
+  {
+    time: "00:02:18",
+    text: " Now baby I believe this is real ",
+  },
+  {
+    time: "00:02:22",
+    text: " So take a chance ",
+  },
+  {
+    time: "00:02:23",
+    text: " And don't ever look back ",
+  },
+  {
+    time: "00:02:26",
+    text: " Don't ever look back ",
+  },
+  {
+    time: "00:02:27",
+    text: " I'ma get your heart racing ",
+  },
+  {
+    time: "00:02:30",
+    text: " In my skin-tight jeans ",
+  },
+  {
+    time: "00:02:32",
+    text: " Be your teenage dream tonight ",
+  },
+  {
+    time: "00:02:35",
+    text: " Let you put your hands on me ",
+  },
+  {
+    time: "00:02:38",
+    text: " In my skin-tight jeans ",
+  },
+  {
+    time: "00:02:40",
+    text: " Be your teenage dream tonight ",
+  },
+  {
+    time: "00:02:43",
+    text: " Tonight, tonight, tonight ",
+  },
+  {
+    time: "00:02:46",
+    text: " Tonight, tonight, tonight ",
+  },
+  {
+    time: "00:02:51",
+    text: " You make me ",
+  },
+  {
+    time: "00:02:54",
+    text: " Feel like I'm livin' a teenage dream ",
+  },
+  {
+    time: "00:02:58",
+    text: " The way you turn me on ",
+  },
+  {
+    time: "00:03:00",
+    text: " I can't sleep ",
+  },
+  {
+    time: "00:03:02",
+    text: " Let's run away ",
+  },
+  {
+    time: "00:03:03",
+    text: " And don't ever look back ",
+  },
+  {
+    time: "00:03:06",
+    text: " Don't ever look back, no ",
+  },
+  {
+    time: "00:03:08",
+    text: " My heart stops ",
+  },
+  {
+    time: "00:03:11",
+    text: " When you look at me ",
+  },
+  {
+    time: "00:03:12",
+    text: " Just one touch ",
+  },
+  {
+    time: "00:03:14",
+    text: " Now, baby, I believe this is real ",
+  },
+  {
+    time: "00:03:19",
+    text: " So take a chance ",
+  },
+  {
+    time: "00:03:19",
+    text: " And don't ever look back ",
+  },
+  {
+    time: "00:03:22",
+    text: " Don't ever look back ",
+  },
+  {
+    time: "00:03:23",
+    text: " I'ma get your heart racing ",
+  },
+  {
+    time: "00:03:26",
+    text: " In my skin-tight jeans ",
+  },
+  {
+    time: "00:03:28",
+    text: " Be your teenage dream tonight ",
+  },
+  {
+    time: "00:03:31",
+    text: " Let you put your hands on me ",
+  },
+  {
+    time: "00:03:34",
+    text: " In my skin-tight jeans ",
+  },
+  {
+    time: "00:03:36",
+    text: " Be your teenage dream tonight ",
+  },
+  {
+    time: "00:03:39",
+    text: " Tonight, tonight, tonight ",
+  },
+  {
+    time: "00:03:42",
+    text: " Tonight, tonight, tonight ",
+  },
+];
+
+const transcript2 = [
+  {
+    time: "00:00:00",
+    text: "I’m sorry, but I don’t want to be an emperor. That’s not my business.",
+  },
+
+  {
+    time: "00:00:05",
+    text: "I don’t want to rule or conquer anyone. I should like to help everyone - if possible - ",
+  },
+  {
+    time: "00:00:10",
+    text: "Jew, Gentile - black man – white. We all want to help one another.",
+  },
+
+  {
+    time: "00:00:15",
+    text: "Human beings are like that. ",
+  },
+  {
+    time: "00:00:17",
+    text: "We want to live by each other’s happiness - not by each other’s misery.",
+  },
+
+  {
+    time: "00:00:20",
+    text: "We don’t want to hate and despise one another. In this world there is room for everyone.",
+  },
+
+  {
+    time: "00:00:24",
+    text: "And the good earth is rich and can provide for everyone. ",
+  },
+  {
+    time: "00:00:27",
+    text: "The way of life can be free and beautiful, but we have lost the way. ",
+  },
+  {
+    time: "00:00:32",
+    text: "Greed has poisoned men’s souls, has barricaded the world with hate, ",
+  },
+  {
+    time: "00:00:37",
+    text: "has goose-stepped us into misery and bloodshed.",
+  },
+
+  {
+    time: "00:00:40",
+    text: "We have developed speed, but we have shut ourselves in. ",
+  },
+  {
+    time: "00:00:43",
+    text: "Machinery that gives abundance has left us in want.",
+  },
+
+  {
+    time: "00:00:46",
+    text: "Our knowledge has made us cynical. Our cleverness, hard and unkind.",
+  },
+
+  {
+    time: "00:00:50",
+    text: "We think too much and feel too little. ",
+  },
+  {
+    time: "00:00:53",
+    text: "More than machinery we need humanity. ",
+  },
+  {
+    time: "00:00:56",
+    text: "More than cleverness we need kindness and gentleness. ",
+  },
+  {
+    time: "00:00:59",
+    text: "Without these qualities, life will be violent and all will be lost.",
+  },
+
+  {
+    time: "00:01:03",
+    text: "The aeroplane and the radio have brought us closer together.",
+  },
+
+  {
+    time: "00:01:06",
+    text: "The very nature of these inventions cries out for the goodness in men - ",
+  },
+  {
+    time: "00:01:09",
+    text: "cries out for universal brotherhood - for the unity of us all. ",
+  },
+  {
+    time: "00:01:12",
+    text: "Even now my voice is reaching millions throughout the world -",
+  },
+
+  {
+    time: "00:01:15",
+    text: "millions of despairing men, women, and little children - ",
+  },
+  {
+    time: "00:01:18",
+    text: "victims of a system that makes men torture and imprison innocent people. ",
+  },
+  {
+    time: "00:01:23",
+    text: "To those who can hear me, I say - do not despair.",
+  },
+
+  {
+    time: "00:01:27",
+    text: "The misery that is now upon us is but the passing of greed - ",
+  },
+  {
+    time: "00:01:30",
+    text: "the bitterness of men who fear the way of human progress. ",
+  },
+  {
+    time: "00:01:34",
+    text: "The hate of men will pass, and dictators die, ",
+  },
+  {
+    time: "00:01:37",
+    text: "and the power they took from the people will return to the people. ",
+  },
+  {
+    time: "00:01:40",
+    text: "And so long as men die, liberty will never perish. ",
+  },
+  {
+    time: "00:01:45",
+    text: "Soldiers! don’t give yourselves to brutes - men who despise you - enslave you -",
+  },
+
+  {
+    time: "00:01:50",
+    text: "who regiment your lives - tell you what to do - what to think and what to feel! ",
+  },
+  {
+    time: "00:01:54",
+    text: "Who drill you - diet you - treat you like cattle, use you as cannon fodder. ",
+  },
+  {
+    time: "00:01:58",
+    text: "Don’t give yourselves to these unnatural men - ",
+  },
+  {
+    time: "00:02:01",
+    text: "machine men with machine minds and machine hearts! ",
+  },
+  {
+    time: "00:02:04",
+    text: "You are not machines! You are not cattle! You are men! ",
+  },
+  {
+    time: "00:02:08",
+    text: "You have the love of humanity in your hearts! ",
+  },
+  {
+    time: "00:02:10",
+    text: "You don’t hate! Only the unloved hate - the unloved and the unnatural!",
+  },
+
+  {
+    time: "00:02:14",
+    text: "Soldiers! Don’t fight for slavery! Fight for liberty! ",
+  },
+  {
+    time: "00:02:18",
+    text: "In the 17th Chapter of St Luke it is written: ",
+  },
+  {
+    time: "00:02:20",
+    text: "“the Kingdom of God is within man” - not one man nor a group of men, but in all men!",
+  },
+
+  {
+    time: "00:02:24",
+    text: "In you! You, the people have the power - the power to create machines. ",
+  },
+  {
+    time: "00:02:28",
+    text: "The power to create happiness! ",
+  },
+  {
+    time: "00:02:30",
+    text: "You, the people, have the power to make this life free and beautiful,",
+  },
+
+  {
+    time: "00:02:33",
+    text: "to make this life a wonderful adventure. ",
+  },
+  {
+    time: "00:02:36",
+    text: "Then - in the name of democracy - let us use that power - let us all unite. ",
+  },
+  {
+    time: "00:02:40",
+    text: "Let us fight for a new world - a decent world that will give men a chance to work - ",
+  },
+  {
+    time: "00:02:45",
+    text: "that will give youth a future and old age a security. ",
+  },
+  {
+    time: "00:02:48",
+    text: "By the promise of these things, brutes have risen to power. But they lie! ",
+  },
+  {
+    time: "00:02:52",
+    text: "They do not fulfil that promise. ",
+  },
+  {
+    time: "00:02:54",
+    text: "They never will! Dictators free themselves but they enslave the people!",
+  },
+
+  {
+    time: "00:02:59",
+    text: "Now let us fight to fulfil that promise! Let us fight to free the world - ",
+  },
+  {
+    time: "00:03:04",
+    text: "to do away with national barriers - to do away with greed, with hate and intolerance. ",
+  },
+  {
+    time: "00:03:10",
+    text: "Let us fight for a world of reason, a world where science and progress will lead to all men’s happiness. ",
+  },
+  {
+    time: "00:03:16",
+    text: "Soldiers! In the name of democracy, let us all unite! ",
+  },
+];
+
+module.exports = [transcript1, transcript2];
